585761da25b85bc9434e565986f090fb3ef23072
MohanapriyaKumarMaragadham <37336090+MohanapriyaKumarMaragadham@users.noreply.github.com>
2018-03-18 15:35:18 +0530

    Update Prime number or not.py

--- Code Kata/Prime number or not.py
+++ Code Kata/Prime number or not.py
@@ -1,13 +1,16 @@
 n=raw_input()
-a=int(n)
 r=0
-for i in (2,a/2):
-	if(a%i)==0:
-		r=1
-		break
+if n.isdigit():
+	a=int(n)
+	for i in (2,a/2):
+		if(a%i)==0:
+			r=1
+			break
+		else:
+			r=0
+	if(r==0):
+		print "yes"
 	else:
-		r=0
-if(r==0):
-	print "yes"
+		print "no"
 else:
-	print "no"
+	print "invalid"
