97c5fcc0656d448ac1086e0bd21da1aec64f0fad
MohanapriyaKumarMaragadham <37336090+MohanapriyaKumarMaragadham@users.noreply.github.com>
2018-04-10 18:59:56 +0530

    Update comparing two strings without strcmp.py

--- Code Kata/comparing two strings without strcmp.py
+++ Code Kata/comparing two strings without strcmp.py
@@ -2,6 +2,7 @@ a,b=raw_input().split()
 c=len(a)
 d=len(b)
 i=0
+count=0
 while(c>0):
 	if(a[i]>b[i]):
 		print a
@@ -10,6 +11,13 @@ while(c>0):
 		print b
 		break
 	else:
-		continue
+		count=count+1
+		if(count==c):
+			print a
+			break
+		else:
+			continue
+			
+
 	c=c-1
 	i=i+1
