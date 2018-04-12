1d1cd094cfd6436b0627a0be776548664f906982
MohanapriyaKumarMaragadham <37336090+MohanapriyaKumarMaragadham@users.noreply.github.com>
2018-04-12 21:38:05 +0530

    Create capitalising 1st letter in string.py

---
+++ Code Kata/player/capitalising 1st letter in string.py
@@ -0,0 +1,23 @@
+a,b=raw_input().split()
+z=len(a)
+y=len(b)
+u=a
+v=b
+e=[]
+f=[]
+for i in range(0,z):
+	if(i==0):
+		s=a[i].upper()
+		e.append(s)
+	elif(i>0):
+		e.append(a[i])
+n="".join(e)
+print n
+for i in range(0,y):
+	if(i==0):
+		s=b[i].upper()
+		f.append(s)
+	elif(i>0):
+		f.append(b[i])
+m="".join(f)
+print m
