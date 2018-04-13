f022fc01ece01e84230ee82578d860fbfb8ece69
MohanapriyaKumarMaragadham <37336090+MohanapriyaKumarMaragadham@users.noreply.github.com>
2018-04-13 20:46:27 +0530

    Update sorting repeated no in array.py

--- Code Kata/player/sorting repeated no in array.py
+++ Code Kata/player/sorting repeated no in array.py
@@ -1,24 +1,29 @@
 a=int(raw_input())
 b=raw_input().split()
 s=[]
+count=0
 for i in range(0,a):
 	u=b.count(b[i])
 	if(u>1):
 		s.append(b[i])
-print s
+	else:
+		count=count+1
 k=list(set(s))		
 d=len(k)
-l=[]
-for j in range(0,d):
-	largest=int(k[0])
-	g=len(k)
-	for i in range(1,g):
-		if(largest>int(k[i])):
-			largest=int(k[i])
-	l.append(largest)
-	k.remove(str(largest))
-for i in l:
-	print i,
+if(count==a):
+	print "unique"
+else:
+	l=[]
+	for j in range(0,d):
+		largest=int(k[0])
+		g=len(k)
+		for i in range(1,g):
+			if(largest>int(k[i])):
+				largest=int(k[i])
+		l.append(largest)
+		k.remove(str(largest))
+	for i in l:
+		print i,
 
 
 
