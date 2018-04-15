5ffe2031c8cbe32f1145094e46fcb86c91ac3b05
MohanapriyaKumarMaragadham <37336090+MohanapriyaKumarMaragadham@users.noreply.github.com>
2018-04-15 20:12:42 +0530

    Update changing the middle value.py

--- Code Kata/changing the middle value.py
+++ Code Kata/changing the middle value.py
@@ -4,6 +4,14 @@ k=(b+1)/2
 p=[]
 for i in range(0,b):
 	p.append(a[i])
-p.remove(a[k-1])
-p.insert(k-1,'*')
-print "".join(p)
+if b%2!=0:
+	p.remove(a[k-1])
+	p.insert(k-1,'*')
+	print "".join(p)
+else:
+	p.remove(a[k-1])
+	p.remove(a[k])
+	p.insert(k-1,'*')
+	p.insert(k,'*')
+	print "".join(p)
+	
