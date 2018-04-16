914e0025776d3a46bd1554da259f1fc174b18534
MohanapriyaKumarMaragadham <37336090+MohanapriyaKumarMaragadham@users.noreply.github.com>
2018-04-16 20:35:08 +0530

    Update anagram.py

--- Code Kata/player/anagram.py
+++ Code Kata/player/anagram.py
@@ -1,4 +1,5 @@
-z=['K','a','b','a','l','i']
+z=['k','a','b','a','l','i']
+u=list(set(z))
 a=int(raw_input())
 c=[]
 count=0
@@ -6,12 +7,18 @@ for i in range(0,a):
 	b=raw_input()
 	c.append(b)
 for i in range(0,a):
+	n=[]
 	g=len(c[i])
 	w=c[i]
-	f=1
+	f=0
 	for j in range(0,g):
 		if w[j] in z:
-			f=f+1
-	if f==len(z):
-		count=count+1
+			n.append(w[j])
+	m=list(set(n))
+	if len(m)==len(u):
+		for k in range(0,len(m)):
+			if m[k] in u:
+				f=f+1
+			if f==len(u):
+				count=count+1
 print count
