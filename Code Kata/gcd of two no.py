fed754aaa341589a26789abbaff19630256bd41d
MohanapriyaKumarMaragadham <37336090+MohanapriyaKumarMaragadham@users.noreply.github.com>
2018-04-17 21:30:47 +0530

    Create gcd of two no.py

---
+++ Code Kata/gcd of two no.py
@@ -0,0 +1,16 @@
+x,y=raw_input().split()
+x=int(x)
+y=int(y)
+a=[]
+b=[]
+c=[]
+for i in range(1,x+1):
+	if x%i==0:
+		a.append(i)
+for i in range(1,y+1):
+	if y%i==0:
+		b.append(i)
+for i in a:
+	if i in b:
+		c.append(i)
+print max(c)
