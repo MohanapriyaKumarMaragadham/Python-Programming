62354fcb25aa23563bfc74e556b2432d9a77f471
MohanapriyaKumarMaragadham <37336090+MohanapriyaKumarMaragadham@users.noreply.github.com>
2018-04-26 21:33:41 +0530

    Update first most repeated digit.py

--- hunter/first most repeated digit.py
+++ hunter/first most repeated digit.py
@@ -1,12 +1,26 @@
 a=int(raw_input())
 b=raw_input().split()
-k=0
-for i in range(0,a):
-	s=b.count(b[i])
-	if s>1:
-		print b[i]
-		break
-	else:
-		k=k+1
-if k==a:
-	print "unique"
+def repeated():
+	k=0
+	for i in range(0,a):
+		s=b.count(b[i])
+		if s>1:
+			print b[i]
+			break
+		else:
+			k=k+1
+	if k==a:
+		print "unique"
+
+if a==len(b):
+	repeated()
+elif a>len(b):
+	a=len(b)
+	repeated()
+else:
+	j=len(b)-a
+	for i in range(1,j+1):
+		b.remove(b[-i])
+	repeated()
+	
+	
