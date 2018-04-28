70e3e9a7c89177567371a19531f3ea523dc4ae51
MohanapriyaKumarMaragadham <37336090+MohanapriyaKumarMaragadham@users.noreply.github.com>
2018-04-28 22:38:44 +0530

    Update common elements in A and B.py

--- Code Kata/player/common elements in A and B.py
+++ Code Kata/player/common elements in A and B.py
@@ -2,9 +2,15 @@ a=int(raw_input())
 b=raw_input().split()
 c=raw_input().split()
 z=[]
-for i in range(0,a):
-	if b[i] in c:
-		z.append(b[i])
-h=list(set(z))
-for i in range(0,len(h)):
-	print h[i],
+if len(b)==len(c)==a:
+	for i in range(0,a):
+		if c[i].isdigit() and b[i].isdigit():
+			if b[i] in c:
+				z.append(b[i])
+		else:
+			print "invalid"
+			break
+	h=list(set(z))
+	for i in range(0,len(h)):
+		print h[i],
+		
