3a41be544e3685668d00248e47431cb29ca024ca
MohanapriyaKumarMaragadham <37336090+MohanapriyaKumarMaragadham@users.noreply.github.com>
2018-05-01 09:28:41 +0000

    Update mul string of size n.py

--- hunter/mul string of size n.py
+++ hunter/mul string of size n.py
@@ -2,16 +2,26 @@ import re
 a=raw_input()
 l=[]
 w=[]
-k=re.findall("\d+",a)
-for i in range(0,len(a)):
-	if a[i].isdigit():
-		continue
-	else:
-		l.append(a[i])
-if len(l)==len(k):
+def mul():
 	for i in range(0,len(l)):
 		u=l[i]*int(k[i])
 		w.append(u)
 	print "".join(w)
+if len(a)<100000:
+	k=re.findall("\d+",a)
+	for i in range(0,len(a)):
+		if a[i].isdigit():
+			continue
+		else:
+			l.append(a[i])
+	if len(l)==len(k):
+		mul()
+	else:
+		d=len(l)-len(k)
+		p=1
+		for j in range(1,d+1):
+			l.remove(l[-p])
+		mul()
 else:
 	print "invalid"
+		
