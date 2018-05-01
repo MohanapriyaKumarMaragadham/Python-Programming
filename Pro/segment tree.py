7e6b9f5bd61fe7396352f1a193c85f0fb10f7336
MohanapriyaKumarMaragadham <37336090+MohanapriyaKumarMaragadham@users.noreply.github.com>
2018-05-01 23:29:03 +0530

    Create segment tree.py

---
+++ Pro/segment tree.py
@@ -0,0 +1,29 @@
+a,b=raw_input().split()
+if a.isdigit() and b.isdigit():
+	a=int(a)
+	b=int(b)
+	w=0
+	c=raw_input().split()
+	for i in range(0,len(c)):
+		if c[i].isdigit():
+			w=w+1
+	if w==len(c):
+		l=[]
+		for i in range(0,b):
+			p=raw_input().split()
+			l.append(p)
+		for i in range(0,len(l)):
+			r=l[i]
+			s=0
+			if r[0].isdigit() and r[1].isdigit():
+				for i in range(int(r[0])-1,int(r[1])):
+					s=s+int(c[i])
+				print s
+			else:
+				continue
+	else:
+		print "invalid"
+else:
+	print "Invalid"
+			
+		
