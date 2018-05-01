8cf92f073511bc3135eb83fad11f5614d9f4ef47
MohanapriyaKumarMaragadham <37336090+MohanapriyaKumarMaragadham@users.noreply.github.com>
2018-05-01 22:33:17 +0530

    Update unique character in array.py

--- hunter/unique character in array.py
+++ hunter/unique character in array.py
@@ -3,13 +3,10 @@ if a.isdigit():
 	a=int(a)
 	b=raw_input().split()
 	for i in range(0,a):
-		if b[i].isdigit():
-			f=b.count(b[i])
-			if f==1:
-				print int(b[i])
-			else:
-				continue
+		f=b.count(b[i])
+		if f==1:
+			print b[i]
 		else:
-			break
+			continue
 else:
 	print "invalid"
