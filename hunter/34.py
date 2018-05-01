91a264d116eaf1b647b330f08ba1df8a4b1c82f0
MohanapriyaKumarMaragadham <37336090+MohanapriyaKumarMaragadham@users.noreply.github.com>
2018-05-01 06:51:15 +0000

    Update 34.py

--- hunter/34.py
+++ hunter/34.py
@@ -1,10 +1,10 @@
 a1=raw_input()
 b1=[]
-l=[]
-for i in range(0,len(a1)):
-	b1.append(a1[i])
-res1=[]
-for i in b1:
-	if i not in res1:
-		res1.append(i)
-print "".join(res1)
+if len(a1)<100000:
+	for i in range(0,len(a1)):
+		b1.append(a1[i])
+	res1=[]
+	for i in b1:
+		if i not in res1:
+			res1.append(i)
+	print "".join(res1)
