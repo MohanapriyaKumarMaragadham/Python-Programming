4ba032dea1e5b2183c32ba74258cf54902d4ef6e
MohanapriyaKumarMaragadham <37336090+MohanapriyaKumarMaragadham@users.noreply.github.com>
2018-05-05 22:07:16 +0530

    Create longest palindrome.py

---
+++ Pro/longest palindrome.py
@@ -0,0 +1,14 @@
+a=raw_input()
+b=a[::-1]
+if a==b:
+	print "0"
+else:
+	s=0
+	l=[]
+	for i in range(0,len(a)):
+		l.append(a[i])
+	res=[]
+	for i in l:
+		if l.count(i)==1:
+			res.append(i)
+	print len(res)
