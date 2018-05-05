a155ef8a9667c63a8e822789a2e45d271e71f476
MohanapriyaKumarMaragadham <37336090+MohanapriyaKumarMaragadham@users.noreply.github.com>
2018-05-06 00:24:00 +0530

    Create diagonal adding.py

---
+++ hunter/diagonal adding.py
@@ -0,0 +1,15 @@
+a=raw_input()
+if a.isdigit():
+	a=int(a)
+	l=[]
+	s=0
+	for i in range(0,a):
+		b=raw_input().split()
+		l.append(b)
+	for i in range(0,a):
+		h=l[i]
+		for j in range(0,len(h)):
+			if i==j:
+				s=s+int(h[j])
+	print s
+		
