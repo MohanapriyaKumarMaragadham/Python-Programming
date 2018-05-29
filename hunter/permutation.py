654b1f5ec6d8772340297740230f06eb439cef4c
MohanapriyaKumarMaragadham <37336090+MohanapriyaKumarMaragadham@users.noreply.github.com>
2018-05-29 15:04:20 +0530

    Create permutation.py

---
+++ hunter/permutation.py
@@ -0,0 +1,22 @@
+def permutation(a):
+	if len(a)==0:
+		return []
+	elif len(a)==1:
+		return [a]
+	else:
+		l=[]
+		for i in range(len(a)):
+			x=a[i]
+			xs=a[:i]+a[i+1:]
+			for p in permutation(xs):
+				l.append([x]+p)
+		return l
+a=raw_input()
+if a.isdigit():
+	data=list(a)
+	w=[]
+	for p in permutation(data):
+		if "".join(p) not in w:
+			w.append("".join(p))
+	for i in w:
+		print i
