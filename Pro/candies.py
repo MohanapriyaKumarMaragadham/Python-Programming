977309c48e1fe875bc3a53585cbee974419cfc49
MohanapriyaKumarMaragadham <37336090+MohanapriyaKumarMaragadham@users.noreply.github.com>
2018-05-30 17:51:51 +0530

    Update candies.py

--- Pro/candies.py
+++ Pro/candies.py
@@ -8,9 +8,5 @@ if a.isdigit():
 			l.append(1)
 		for i in range(0,a-1):
 			if int(b[i+1])>int(b[i]):
-				if l[i+1]<=l[i]:
-					l[i+1]=l[i+1]+l[i]
-			elif int(b[i])>int(b[i+1]):
-				if l[i]<=l[i+1]:
-					l[i]=l[i]+l[i+1]
+				l[i+1]=l[i+1]+l[i]
 		print sum(l)
