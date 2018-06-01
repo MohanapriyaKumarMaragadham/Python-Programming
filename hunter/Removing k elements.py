2a6d94689997404960e24714d46805e3821ddd86
MohanapriyaKumarMaragadham <37336090+MohanapriyaKumarMaragadham@users.noreply.github.com>
2018-06-01 19:20:07 +0530

    Update Removing k elements.py

--- hunter/Removing k elements.py
+++ hunter/Removing k elements.py
@@ -1,14 +1,15 @@
 a,b=raw_input().split()
-a=int(a)
-c=raw_input().split()
-l=[]
-for i in range(a):
-	if c[i]==b:
-		continue
+if a.isdigit() and b.isdigit():
+	a=int(a)
+	c=raw_input().split()
+	l=[]
+	for i in range(a):
+		if c[i]==b:
+			continue
+		else:
+			l.append(c[i])
+	if len(l)==0:
+		print "empty"
 	else:
-		l.append(c[i])
-if len(l)==0:
-	print "empty"
-else:
-	for i in range(len(l)):
-		print l[i],
+		for i in range(len(l)):
+			print l[i],
